1405eae02ea6e06c573ff0d3e294ba06fd042325
TrondinDS <99561627+TrondinDS@users.noreply.github.com>
2023-03-26 13:50:22 +0300

    Matrix

--- src/math/Mx.cpp
+++ src/math/Mx.cpp
@@ -3,30 +3,7 @@
 #include <sstream>
 Mx::Mx(std::string& text)
 {
-    std::vector<std::vector<int>> res;
-    std::stringstream ss(text);
-    char c;
-    while (ss >> c) {
-        std::vector<int> row;
-        int val;
-        while (ss >> val) {
-            row.push_back(val);
-
-            if (ss.peek() == ')') {
-                ss.ignore();
-                break;
-            }
-        }
-        res.push_back(row);
-    }
-    data.assign(res.begin(), res.end());
-    for (auto row : data) {
-        for (auto val : row) {
-            std::cout << val << " "; C++;
-        }
-        std::cout << std::endl; R++;
-    }
-    C = C / R;
+    std::vector<std::vector<int>> res = parse_matrix(text);
 }
 
 Mx::~Mx()
@@ -35,15 +12,14 @@ Mx::~Mx()
 
 std::string Mx::result() const
 {
-    std::string str = "";
-    for (auto row : data) {
-        str += "(";
-        for (auto val : row) {
-            str += " " + (std::to_string(val));
+    std::string ResultStr = "";
+    for (int i = 0; i < Result.size(); i++) {
+        for (int j = 0; j < Result[0].size(); j++) {
+            ResultStr += std::to_string(Result[i][j]) + " ";
         }
-        str += " )\n";
+        ResultStr += "\n";
     }
-    return str;
+    return ResultStr;
 }
 
 int Mx::get_data() const
@@ -51,16 +27,123 @@ int Mx::get_data() const
     return 0;
 }
 
+Calculatable* Mx::add(Calculatable* other)
+{
+    Result = addMatrices(data, ((Mx*)other)->data);
+    return this;
+}
+
+Calculatable* Mx::sub(Calculatable* other)
+{
+    Result = subtractMatrices(data, ((Mx*)other)->data);
+    return this;
+}
+
 Calculatable* Mx::mul(Calculatable* other)
 {
-    std::vector<std::vector<int>> Result;
-    for(int i = 0; i < R;i++)
-        for (int j = 0; j < ((Mx*)other)->C; j++)
-        {
-            Result[i][j] = 0;
-            for (int k = 0; k <= ((Mx*)other)->C + 1; k++)
-                Result[i][j] += (data[i][k] * ((Mx*)other)->data[k][j]);
+    Result = multiplyMatrices(data, ((Mx*)other)->data);
+    return this;
+}
+
+Calculatable* Mx::div(Calculatable* other)
+{
+    Result = divideMatrixByNumber(data, ((Mx*)other)->data[0][0]);
+    return this;
+}
+
+std::vector<std::vector<int>> Mx::parse_matrix(std::string s)
+{
+    std::vector<std::vector<int>> result;
+    std::stringstream ss(s);
+    char c;
+
+    while (ss >> c) {
+        std::vector<int> row;
+        int val;
+
+        while (ss >> val) {
+            row.push_back(val);
+
+            if (ss.peek() == ')') {
+                ss.ignore();
+                break;
+            }
         }
-    data.assign(Result.begin(),Result.end());
-    C = ((Mx*)other)->C;
+
+        result.push_back(row);
+    }
+    return result;
+}
+
+std::vector<std::vector<int>> Mx::multiplyMatrices(const std::vector<std::vector<int>>& A, const std::vector<std::vector<int>>& B)
+{
+    // Проверяем, что матрицы можно перемножить
+    if (A[0].size() != B.size()) {
+        std::cout << "Error: The matrices cannot be multiplied!" << std::endl;
+        return {};
+    }
+    // Инициализируем результат
+    std::vector<std::vector<int>> C(A.size(), std::vector<int>(B[0].size(), 0));
+    // Перемножаем матрицы
+    for (int i = 0; i < A.size(); i++) {
+        for (int j = 0; j < B[0].size(); j++) {
+            for (int k = 0; k < B.size(); k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+    // Возвращаем результат
+    return C;
+}
+
+std::vector<std::vector<int>> Mx::addMatrices(const std::vector<std::vector<int>>& A, const std::vector<std::vector<int>>& B)
+{
+    // Проверяем, что матрицы имеют одинаковый размер
+    if (A.size() != B.size() || A[0].size() != B[0].size()) {
+        std::cout << "Error: The matrices have different sizes!" << std::endl;
+        return {};
+    }
+    // Инициализируем результат
+    std::vector<std::vector<int>> C(A.size(), std::vector<int>(A[0].size(), 0));
+    // Складываем матрицы
+    for (int i = 0; i < A.size(); i++) {
+        for (int j = 0; j < A[0].size(); j++) {
+            C[i][j] = A[i][j] + B[i][j];
+        }
+    }
+    // Возвращаем результат
+    return C;
+}
+
+std::vector<std::vector<int>> Mx::subtractMatrices(const std::vector<std::vector<int>>& A, const std::vector<std::vector<int>>& B)
+{
+    // Проверяем, что матрицы имеют одинаковый размер
+    if (A.size() != B.size() || A[0].size() != B[0].size()) {
+        std::cout << "Error: The matrices have different sizes!" << std::endl;
+        return {};
+    }
+    // Инициализируем результат
+    std::vector<std::vector<int>> C(A.size(), std::vector<int>(A[0].size(), 0));
+    // Вычитаем матрицы
+    for (int i = 0; i < A.size(); i++) {
+        for (int j = 0; j < A[0].size(); j++) {
+            C[i][j] = A[i][j] - B[i][j];
+        }
+    }
+    // Возвращаем результат
+    return C;
+}
+
+std::vector<std::vector<int>> Mx::divideMatrixByNumber(const std::vector<std::vector<int>>& A, int num)
+{
+    // Инициализируем результат
+    std::vector<std::vector<int>> C(A.size(), std::vector<int>(A[0].size(), 0));
+    // Делим матрицу на число
+    for (int i = 0; i < A.size(); i++) {
+        for (int j = 0; j < A[0].size(); j++) {
+            C[i][j] = A[i][j] / num;
+        }
+    }
+    // Возвращаем результат
+    return C;
 }
--- src/math/Mx.h
+++ src/math/Mx.h
@@ -13,13 +13,19 @@ public:
 	std::vector<std::vector<int>> Result;
 	Mx(std::string& text);
 	~Mx();
-	std::string result() const;
 
+	std::vector<std::vector<int>> parse_matrix(std::string s);
+	std::vector<std::vector<int>> multiplyMatrices(const std::vector<std::vector<int>>& A, const std::vector<std::vector<int>>& B);
+	std::vector<std::vector<int>> addMatrices(const std::vector<std::vector<int>>& A, const std::vector<std::vector<int>>& B);
+	std::vector<std::vector<int>> subtractMatrices(const std::vector<std::vector<int>>& A, const std::vector<std::vector<int>>& B);
+	std::vector<std::vector<int>> divideMatrixByNumber(const std::vector<std::vector<int>>& A, int num);
+	std::string result() const;
 	int get_data() const;
-	//Calculatable* add(Calculatable* other);
-	//Calculatable* sub(Calculatable* other);
+
+	Calculatable* add(Calculatable* other);
+	Calculatable* sub(Calculatable* other);
 	Calculatable* mul(Calculatable* other);
-	//Calculatable* div(Calculatable* other);
+	Calculatable* div(Calculatable* other);
 	//Calculatable* rtd(Calculatable* other);
 };
 
