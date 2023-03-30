1bce949341ac0fe89698f446136502812d80cf50
mesiac <99199841+messiac@users.noreply.github.com>
2023-03-30 20:41:08 +0300

    final(?) complex

--- src/math/complex.cpp
+++ src/math/complex.cpp
@@ -1,54 +1,123 @@
 #include "complex.h"
 
+//Complex Complex::operator +(const Complex& c)
+//{
+//    Complex d;
+//    d.num.a = num.a + c.num.a;
+//    d.num.b = num.b + c.num.b;
+//    return d;
+//}
+//
+//Complex Complex::operator -(const Complex& c) {
+//    Complex y;
+//    y.num.a = num.a - c.num.a;
+//    y.num.b = num.b - c.num.b;
+//    return y;
+//}
+//
+//Complex Complex::operator *(const Complex& c) {
+//    Complex t;
+//    t.num.a = num.a * c.num.a - num.b * c.num.b;
+//    t.num.b = num.a * c.num.b + num.b * c.num.a;
+//    return t;
+//}
+//Complex Complex::operator /(const Complex& c) {
+//    Complex t;
+//    t.num.a = (num.a * c.num.a + num.b * c.num.b) / (num.a * num.a + num.b * num.b);
+//    t.num.b = (num.b * c.num.a - num.a * c.num.b) / (num.a * num.a + num.b * num.b);
+//    return t;
+//}
+//
+//bool Complex::operator ==(const Complex& c) {
+//    if ((num.a == c.num.a) && (num.b == c.num.b))
+//        return true;
+//    return false;
+//}
 
-
-Complex Complex::operator +(const Complex& c) 
+complex AddCx(const complex& a, const complex& b)
 {
-    Complex d;
-    d.num.a = num.a + c.num.a;
-    d.num.b = num.b + c.num.b;
+    complex d;
+    d.a = a.a + b.a;
+    d.b = a.b + b.b;
     return d;
 }
 
-Complex Complex::operator -(const Complex& c) {
-    Complex y;
-    y.num.a = num.a - c.num.a;
-    y.num.b = num.b - c.num.b;
-    return y;
+//Cx AddCx(const Cx& a, const Cx& b)
+//{
+//    Cx d;
+//    d.num.a = a.num.a + b.num.a;
+//    d.num.b = a.num.b + b.num.b;
+//    return d;
+//}
+
+complex SubCx(const complex& a, const complex& b)
+{
+    complex d;
+    d.a = a.a - b.a;
+    d.b = a.b - b.b;
+    return d;
 }
 
-Complex Complex::operator *(const Complex& c) {
-    Complex t;
-    t.num.a = num.a * c.num.a - num.b * c.num.b;
-    t.num.b = num.a * c.num.b + num.b * c.num.a;
-    return t;
-}
-Complex Complex::operator /(const Complex& c) {
-    Complex t;
-    t.num.a = (num.a * c.num.a + num.b * c.num.b) / (num.a * num.a + num.b * num.b);
-    t.num.b = (num.b * c.num.a - num.a * c.num.b) / (num.a * num.a + num.b * num.b);
-    return t;
+complex MulCx(const complex& a, const complex& b)
+{
+    complex d;
+    d.a = a.a * b.a - a.b * b.b;
+    d.b = a.a * b.b + a.b * b.a;
+    return d;
 }
 
-bool Complex::operator ==(const Complex& c) {
-    if ((num.a == c.num.a) && (num.b == c.num.b))
-        return true;
-    return false;
+complex DivCx(const complex& a, const complex& b)
+{
+    complex d;
+    d.a = (a.a * b.a + a.b * b.b) / (a.a * a.a + a.b * a.b);
+    d.b = (a.b * b.a - a.a * b.b) / (a.a * a.a + a.b * a.b);
+    return d;
 }
 
-void Complex::Print() {
+
+void Cx::Print() {
     if (num.b >= 0)
         cout << num.a << "+" << num.b << "i\n";
     else
         cout << num.a << num.b << "i\n";
 }
 
-Complex Complex::Read() {
-    double x, y;
-    cout << "Действительнвая часть: ";
-    cin >> x;
-    cout << "Мнимая часть:        : ";
-    cin >> y;
-    Complex A = Complex(x, y);
-    return A;
+//Cx Cx::Read() {
+//    double x, y;
+//    cout << "Действительнвая часть: ";
+//    cin >> x;
+//    cout << "Мнимая часть:        : ";
+//    cin >> y;
+//    Cx A = Cx(x, y);
+//    return A;
+//}
+
+std::string Cx::result() const
+{
+    std::string ResultStr = std::to_string(Result.a) + " " + std::to_string(Result.b) + "i";;
+    return ResultStr;   
+}
+
+Calculatable* Cx::add(Calculatable* other)
+{
+    Result = AddCx(num, ((Cx*)other)->num);
+    return this;
+}
+
+Calculatable* Cx::sub(Calculatable* other)
+{
+    Result = SubCx(num, ((Cx*)other)->num);
+    return this;
+}
+
+Calculatable* Cx::mul(Calculatable* other)
+{
+    Result = MulCx(num, ((Cx*)other)->num);
+    return this;
+}
+
+Calculatable* Cx::div(Calculatable* other)
+{
+    Result = DivCx(num, ((Cx*)other)->num);
+    return this;
 }
--- src/math/complex.h
+++ src/math/complex.h
@@ -1,6 +1,11 @@
 # include <iostream>
 # include <stdlib.h>
 # include <conio.h>;
+#include "Calculatable.hpp"
+#include "../syntax/Token.hpp"
+#include "Fraction.hpp"
+#include <string>
+
 
 using namespace std;
 
@@ -8,19 +13,33 @@ struct complex {
    int a, b;
 };
 
-class Complex {
-    complex num;
+class Cx : public Calculatable {
+  
 public:
-    Complex(int x = 0, int y = 0)
+    complex num;
+    complex Result;
+    Cx(int x = 0, int y = 0)
     {
         num.a = x;
         num.b = y;
     }
-    Complex operator +(const Complex& c);
+   /* Complex operator +(const Complex& c);
     Complex operator *(const Complex& c);
     Complex operator -(const Complex& c);
     Complex operator /(const Complex& c);
-    bool operator ==(const Complex& c);
+    bool operator ==(const Complex& c);*/
+
+    Calculatable* add(Calculatable* other);
+    Calculatable* sub(Calculatable* other);
+    Calculatable* mul(Calculatable* other);
+    Calculatable* div(Calculatable* other);
+    std::string result() const;
+
+
+    //Cx SubCx(const Cx& a, const Cx& b);
+
     void Print();
-    Complex Read();
+    Cx Read();
 };
+
+
