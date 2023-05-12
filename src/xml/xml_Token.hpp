46ab09d4b62fe542beee914f41bbd88318bba0f6
Krai53n <greasha46@gmail.com>
2023-05-12 21:01:25 +0300

    fix typo https://github.com/kra53n/calc/issues/14 and replace discription of token var

--- src/xml/xml_Token.hpp
+++ src/xml/xml_Token.hpp
@@ -14,13 +14,13 @@ struct Token {
     Val, // <tag_name>some elem</tag_name>
          //           ~~~~~~~~~ - Val
     Attr, // <tag_name attr1="some info"               attr2></tag_name>
-          //           ~~~~~~~~~~~~~~~~~ - Attribute - ~~~~~
+          //           ~~~~~~~~~~~~~~~~ - Attributes - ~~~~~
   };
 
   TokenName name; 
   std::string lexeme;
   int row;
-  int col; // where lexeme begins
+  int col; // which character in the string does the token begin with
 
   Token(TokenName name, std::string lexeme, int row, int col);
 };
